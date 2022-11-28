90a0b2b0cd16897b91de4a5444d9cc300007b41d
webreflection <andrea.giammarchi@gmail.com>
2022-11-28 13:11:54 +0100

    v0.2 with perf and structural changes

--- test/transform/output.js
+++ test/transform/output.js
@@ -3,41 +3,21 @@ var _templateReference = {},
   _templateReference3 = {},
   _templateReference4 = {};
 import ESXToken from "@ungap/esxtoken";
-const div = ESXToken.template(
-  _templateReference,
-  ESXToken.element("div", null)
-);
-const div2 = ESXToken.template(
+const div = ESXToken.e(_templateReference, "div", ESXToken._);
+const div2 = ESXToken.e(
   _templateReference2,
-  ESXToken.element(
-    "div",
-    ESXToken.create(ESXToken.MIXED_TYPE, [
-      ESXToken.property(ESXToken.STATIC_TYPE, "a", "a"),
-      ESXToken.property(ESXToken.RUNTIME_TYPE, "b", "b"),
-    ]),
-    ESXToken.create(
-      ESXToken.STATIC_TYPE,
-      ESXToken.element("p", null, ESXToken.create(ESXToken.STATIC_TYPE, "c"))
-    )
-  )
+  "div",
+  [ESXToken.a(false, "a", "a"), ESXToken.a(true, "b", "b")],
+  [ESXToken.e(null, "p", ESXToken._, [ESXToken.s("c")])]
 );
 function MyComponent(...args) {
-  return ESXToken.template(
-    _templateReference3,
-    ESXToken.fragment(
-      ESXToken.create(ESXToken.RUNTIME_TYPE, "A"),
-      ESXToken.create(ESXToken.STATIC_TYPE, ","),
-      ESXToken.create(ESXToken.RUNTIME_TYPE, "B")
-    )
-  );
+  return ESXToken.f(_templateReference3, [
+    ESXToken.i("A"),
+    ESXToken.s(","),
+    ESXToken.i("B"),
+  ]);
 }
-const component = ESXToken.template(
-  _templateReference4,
-  ESXToken.component(
-    MyComponent,
-    ESXToken.create(ESXToken.RUNTIME_TYPE, [
-      ESXToken.property(ESXToken.STATIC_TYPE, "a", "a"),
-      ESXToken.property(ESXToken.RUNTIME_TYPE, "", props),
-    ])
-  )
-);
+const component = ESXToken.c(_templateReference4, MyComponent, [
+  ESXToken.a(false, "a", "a"),
+  ESXToken.i(props),
+]);
